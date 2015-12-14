2b29dbfaeb15ef38c9ea9ed30a5341e62aa0e742
Mirza Belal Ahmed <mirza360@outlook.com>
2015-12-15 03:15:25 +0600

    update

--- Snake Game/snake.c
+++ Snake Game/snake.c
@@ -294,9 +294,9 @@ void Food()
     {
         length++;
         score++;
-        time_t a;
-        a=time(0);
-        srand(a);
+        //time_t a;
+        //a=time(0);
+        //srand(a);
         food.x=rand()%70;
         if(food.x<=10)
             food.x+=11;
@@ -305,7 +305,7 @@ void Food()
 
             food.y+=11;
     }
-    else if(food.x==0)/*to create food for the first time coz global variable are initialized with 0*/
+    else if(food.x==0)/*to create food for the first time*/
     {
         food.x=rand()%70;
         if(food.x<=10)
