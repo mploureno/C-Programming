a24ebad0914d67d08329fe1cd0de196292eb1300
Mirza Belal Ahmed <mirza360@outlook.com>
2015-12-14 14:10:41 +0600

    Snake

--- Snake Game/snake.c
+++ Snake Game/snake.c
@@ -17,7 +17,6 @@ int length;
 int bend_no;
 int len;
 char key;
-void record();
 void load();
 int life;
 void Delay(long double);
@@ -277,7 +276,7 @@ void ExitGame()
         {
             system("cls");
             printf("All lives completed\nBetter Luck Next Time!!!\nPress any key to quit the game\n");
-            record();
+            //record();
             exit(0);
         }
     }
@@ -449,49 +448,6 @@ void Print()
    printf("\n\nPress any key to play game...");
    if(getch()==27)
    exit(0);
-}
-void record(){
-   char plname[20],nplname[20],cha,c;
-   int i,j,px;
-   FILE *info;
-   info=fopen("record.txt","a+");
-   getch();
-   system("cls");
-   printf("Enter your name\n");
-   scanf("%[^\n]",plname);
-   //************************
-   for(j=0;plname[j]!='\0';j++){ //to convert the first letter after space to capital
-   nplname[0]=toupper(plname[0]);
-   if(plname[j-1]==' '){
-   nplname[j]=toupper(plname[j]);
-   nplname[j-1]=plname[j-1];}
-   else nplname[j]=plname[j];
-   }
-   nplname[j]='\0';
-   //*****************************
-   //sdfprintf(info,"\t\t\tPlayers List\n");
-   fprintf(info,"Player Name :%s\n",nplname);
-    //for date and time
-
-   time_t mytime;
-  mytime = time(NULL);
-  fprintf(info,"Played Date:%s",ctime(&mytime));
-     //**************************
-     fprintf(info,"Score:%d\n",px=Scoreonly());//call score to display score
-     //fprintf(info,"\nLevel:%d\n",10);//call level to display level
-   for(i=0;i<=50;i++)
-   fprintf(info,"%c",'_');
-   fprintf(info,"\n");
-   fclose(info);
-   printf("wanna see past records press 'y'\n");
-   cha=getch();
-   system("cls");
-   if(cha=='y'){
-   info=fopen("record.txt","r");
-   do{
-       putchar(c=getc(info));
-       }while(c!=EOF);}
-     fclose(info);
 }
 
 int Score()
